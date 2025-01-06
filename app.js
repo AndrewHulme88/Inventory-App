7e45f83eab203f28f5ef6c33d0102c62e5024384
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-06 20:51:32 +1100

    Created home.ejs, and added navbar

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/cars', carRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Car Inventory App!');
+  res.render('home');
 });
 
 app.listen(PORT, () => {
