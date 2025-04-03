be724581e96ce1b1067bbab568c1e03c8cdb4884
andrew hulme <andrew_hulme04@hotmail.com>
2025-04-03 16:50:42 +1100

    Commit

---
+++ carRoutesCopy.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const router = express.Router();
+// const carController = require('../controllers/carController');
+
+// router.get('/', carController.viewCars);
+// router.post('/add', carController.addCar);
+// router.post('/delete/:id', carController.deleteCar);
+// router.get('/edit/:id', carController.editCar);
+// router.post('/edit/:id', carController.updateCar);
+// router.get('/add', (req, res) => {
+//   res.render('addCar');
+// });
+
+// module.exports = router;
